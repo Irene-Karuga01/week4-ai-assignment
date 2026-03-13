776673355feb23c9cd5118840433d19bf592f0fa
Irene Karuga <mbithekaruga10@gmail.com>
2026-03-13 04:43:11 -0700

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,37 @@
+# Question 4
+print("Hello AI students")
+
+# Result
+# Hello AI students
+
+# Question 5
+Name = "Irene"
+Age = 20
+City = "Nairobi"
+
+print("Name:",Name)
+print("Age:",Age)
+print("City:",City)
+
+# Result
+# Name: Irene
+# Age: 20
+# City: Nairobi
+
+
+# Question 6
+programming=["python","java","javascript","c++","go"]
+print(programming[0])
+
+# Result
+# python
+
+
+# Question 7
+student={"name":"irene",
+         "age":25,
+         "course":"ai"}
+print(student["name"])
+
+# Result
+# irene
